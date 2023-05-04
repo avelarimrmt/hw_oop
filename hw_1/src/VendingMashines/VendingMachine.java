d4a4e69ff46ded786bae03949fea43d7ae87fce9
Valeria Martynova <kthf175@gmail.com>
2023-05-04 22:29:09 +0500

    first commit

---
+++ hw_1/src/VendingMashines/VendingMachine.java
@@ -0,0 +1,44 @@
+package VendingMashines;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Products.Product;
+
+public class VendingMachine {
+    private int volume;
+    private List<Product> products;
+    private List<String> workLog;
+
+    public VendingMachine(int volume) {
+        this.volume = volume;
+        products = new ArrayList<Product>();
+        workLog = new ArrayList<String>();
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
+    }
+
+    public void addSales(String line)
+    {
+        workLog.add(line);
+    }
+
+    public Product getProdByName(String name)
+    {
+        for(Product prod: products)
+        {
+            if (prod.getName().contains(name))
+            {
+                return prod;
+            }
+        }
+        return null;
+    }
+
+    public List<Product> getProdAll()
+    {
+        return products;
+    }
+}
