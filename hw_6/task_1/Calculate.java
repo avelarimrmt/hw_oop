794babbe783bc71548f5ab7f8a17915d150a71a2
Valeria Martynova <kthf175@gmail.com>
2023-05-28 22:22:06 +0500

    Update Calculate.java

--- hw_6/task_1/Calculate.java
+++ hw_6/task_1/Calculate.java
@@ -5,6 +5,14 @@ public class Calculate {
     /** базовая зп*/
     private int baseSalary;
 
+    /**
+     * конструктор подсчета зп 1 параметр
+     * @param baseSalary базовая зп
+     */
+    public Calculate (int baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
     /** считаем чистую заработную плату*/
     public int calculateNetSalary() {
         int tax = (int) (baseSalary * 0.25);
